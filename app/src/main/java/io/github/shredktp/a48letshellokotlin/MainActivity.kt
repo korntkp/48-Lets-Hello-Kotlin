aeb3cf851f21f47285a0d43c17f6f86802ed496b
shredktp <kornth_ebasicxp@hotmail.com>
2017-02-18 00:14:10 +0700

    Initial commit

---
+++ app/src/main/java/io/github/shredktp/a48letshellokotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.shredktp.a48letshellokotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*;
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_hello.text = getString(R.string.main_tv_hello_kotlin)
+    }
+}
